c7d6d91005c90f252a6a6e29248644696e23048e
Didun Morodolu <elkenzom1@gmail.com>
2019-03-07 10:40:58 +0100

    fix API backend route

--- src/apis/vyStoreBackend.js
+++ src/apis/vyStoreBackend.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+// baseURL:'http://localhost:8000/api/v1',
 export default axios.create({
-  baseURL:'http://localhost:8000/api/v1',
+  baseURL:'https://tia-store-manager.herokuapp.com/api/v1',
   headers:{
     'access-token': localStorage.getItem('authToken')
   }
