a01bb89b8d57f9869d84d102df20ae88f0236180
Didun Morodolu <elkenzom1@gmail.com>
2019-03-07 11:42:54 +0100

    fixes homepagw

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,8 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Home Page</h1>
+        <h1>Vy Store Manager</h1>
+        <p>Welcome to Vy Store Manager, navigate to login link on the menu to get started</p>
       </div>
     )
   }
